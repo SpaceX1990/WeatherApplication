7ef8bb35103eac5aea841d7f388e4bbd4a1f66da
VTischer <viktor.t1@schule.bremen.de>
2025-01-17 11:05:41 +0100

    Api-Call implementiert

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using System.Net.Http;
+using System.Text;
+using System.Text.Json;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -8,6 +10,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WeatherApplication.Model;
 
 namespace WeatherApplication {
     /// <summary>
@@ -18,6 +21,29 @@ namespace WeatherApplication {
             InitializeComponent();
         }
 
+        public static void ReadAndDeserializeAPIResponse() {
+            HttpClient client = new HttpClient() {
+                BaseAddress = new Uri("http://www.7timer.info/bin")
+            };
 
+        //http://www.7timer.info/bin/api.pl?lon=113.17&lat=23.09&product=civil&output=json
+
+            var response = client.GetAsync("/api.pl?lon=113.17&lat=23.09&product=civil&output=json").Result;
+
+            var content = response.Content.ReadAsStringAsync().Result;
+
+            Console.WriteLine("Content:");
+            Console.WriteLine(content);
+            Console.WriteLine();
+
+            var post = JsonSerializer.Deserialize<Root>(content);
+
+            Console.WriteLine("Zufälliger nutzloser Fakt:");
+            Console.WriteLine(post);
+        }
+
+        private void UpdateWeather_Click(object sender, RoutedEventArgs e) {
+            ReadAndDeserializeAPIResponse();
+        }
     }
 }
